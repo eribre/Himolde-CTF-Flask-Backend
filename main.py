4d033ecce7be15e2da060f07de9829a2800f7ba4
Erik Brendehaug <erik.brendehaug@himolde.no>
2023-06-09 04:26:48 +0200

    CORS and further testing

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 import datetime
-  
+
 x = datetime.datetime.now()
-  
+
 # Initializing flask app
 app = Flask(__name__)
-  
-  
+CORS(app)
+
+
+# Route for root api page
+@app.route("/")
+def home():
+    return make_response(jsonify({"success": True}), 200)
+
+
 # Route for seeing a data
-@app.route('/data')
+@app.route("/data")
 def get_time():
-  
     # Returning an api for showing in  reactjs
-    return {
-        'Name':"geek", 
-        "Age":"22",
-        "Date":x, 
-        "programming":"python"
-        }
-  
-      
+    return {"name": "geek", "age": "22", "date": x, "programming": "python"}
+
+
 # Running app
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5000)
